66a31331e11e0fc18d4f07141c1aab1c02a75d9a
Aron <aron.rynkiewicz@gmail.com>
2020-10-30 16:38:55 +0100

    requirements.txt change

--- chem_model/chem_model/settings.py
+++ chem_model/chem_model/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#        'default': {
+#            'ENGINE': 'djongo',
+#            'NAME': 'chem_model',
+#        }
+#    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
